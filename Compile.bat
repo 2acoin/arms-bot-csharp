956088b44718fcc65304fe3e2c816d0c65ba97a1
Miner 2ACoin <miner@2acoin.org>
2018-10-09 15:05:26 -0500

    Update compile for each destination reference

--- Compile.bat
+++ Compile.bat
@@ -1,2 +1,6 @@
-﻿dotnet publish C:\Users\Benjamin\Desktop\tip\TrtlBotSharp\TrtlBotSharp.csproj -c Release -r win10-x64
+﻿rem //linux - dotnet publish {system path to TrtlBotSharp.csproj} -c Release -r linux-x64
+rem //Ubuntu - dotnet publish {system path to TrtlBotSharp.csproj} -c Release -r ubuntu.16.04-x64-x64
+rem // windows7 - dotnet publish {system path to TrtlBotSharp.csproj} -c Release -r win7-x64
+rem // windows10 - dotnet publish {system path to TrtlBotSharp.csproj} -c Release -r win10-x64
+rem // uncomment option as needed
 pause
